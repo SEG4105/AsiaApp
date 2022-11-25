7bd6650a28c1a07e9cb6f90a2ce5fda4a1ab711f
Jack Clarke <jackfulcher09@gmail.com>
2022-11-25 11:32:15 -0500

    setup asia

--- asia/copyFromLocal.sh
+++ asia/copyFromLocal.sh
@@ -1,1 +1,1 @@
-rsync -av ../../LocalApp/local-app/ ./ --exclude README.md --exclude node_modules
+rsync -av ../../LocalApp/local-app/ ./ --exclude README.md --exclude node_modules --exclude src/videoUrl.js
---
+++ asia/src/videoUrl.js
@@ -0,0 +1,2 @@
+
+export const videos_url = "https://asia-f7fkcnh2bpddh6b4.z01.azurefd.net/newcontainer"
